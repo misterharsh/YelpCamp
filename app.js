36afc7b6b8e90ed0fdb1168710d69fce368fdc9d
Harsh Modhera <hkmodhera@gmail.com>
2019-03-05 00:31:59 -0600

    decided to put yelp camp on github.. to document the steps of working through the final stages of the udemy web dev bootcamp course -- the project only has basic stuff done so far

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// Connect to database: yelp_camp
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+
+// Define schema
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Define routes
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+    Campground.find({}, function(err, campgrounds) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: campgrounds});
+        }
+    });
+});
+
+app.post("/campgrounds", function(req, res) {
+    let newCampground = {
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description
+    };
+
+    Campground.create(newCampground, function(err, campground) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+   
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("newCampground");
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground}); 
+        }
+    });
+});
+
+app.listen(3000, function() {
+    console.log("The yelp camp server has started on port 3000...");
+});
